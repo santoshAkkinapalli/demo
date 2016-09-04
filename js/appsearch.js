e977ac31babef5ef8c728ae54802b37a18bf3639
santoshAkkinapalli <sandhyagoals@gmail.com>
2016-09-04 09:58:17 +0530

    initial commit

---
+++ js/appsearch.js
@@ -0,0 +1,33 @@
+function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
+
+$(function() {
+    $("#search").on("change", function(e) {
+       e.preventDefault();
+       // prepare the request
+       var request = gapi.client.youtube.search.list({
+            part: "snippet",
+            type: "video",
+            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+            maxResults: 10,
+            order: "viewCount",
+            publishedAfter: "2015-01-01T00:00:00Z"
+       }); 
+       // execute the request
+       request.execute(function(response) {
+          var results = response.result;
+          $("#results").html("");
+          $.each(results.items, function(index, item) {
+            $.get("item.html", function(data) {
+                $("#results").append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
+            });
+          });
+       });
+    });
+});
+
+function init() {
+    gapi.client.setApiKey("AIzaSyCoZEHU4kzM04x2urcOJjobfCSFmhQP5GM");
+    gapi.client.load("youtube", "v3", function() {
+        // yt api is ready
+    });
+}
